771cc40601f6a9a6e9010279a3a53b4d1cbaff34
Gabriela Salazar <asmgabriela@gmail.com>
2022-07-25 20:03:15 -0500

    feat: install express and initilize server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hola mundo!");
+// Import express
+const express = require("express");
+
+// Variable declarations to start the server.
+// Asign express function to app
+const app = express();
+// Specify the port in which the server will run
+const port = 5000;
+
+app.listen(port, () => {
+  console.log("Initializing server");
+});
