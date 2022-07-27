965d235d189bc21769b5415e8e2c6a063843b415
Gabriela Salazar <asmgabriela@gmail.com>
2022-07-26 19:16:40 -0500

    chore: create new controller to use pokemon service

---
+++ src/controllers/pokemon/index.js
@@ -0,0 +1,5 @@
+// Import pokemonServices from services structure
+const pokemonServices = require("../../services/pokemon/index");
+
+// create a pokemonService using pokemonServices class
+const pokemonService = new pokemonServices();
