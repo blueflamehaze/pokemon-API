62b5aeecc3ecf48dcc6c58d16a1f1b1bc0c50c8c
Gabriela Salazar <asmgabriela@gmail.com>
2022-07-27 21:51:36 -0500

    feat: configure file sfor heroku

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const express = require("express");
 // Asign express function to app
 const app = express();
 // Specify the port in which the server will run
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // Add middleware to use request.body
 app.use(express.json());
